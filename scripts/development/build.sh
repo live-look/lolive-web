c5d7bad4af20448e5ab47117f261ef99f667c531
Andrew N. Shalaev <isqad88@gmail.com>
2020-01-23 17:05:46 +0500

    feature: move docker-compose to development namespace

--- scripts/development/build.sh
+++ scripts/development/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -x
 set -e
+set -x
 
-docker-compose -f deployments/docker-compose.yml build camforchat-app
+docker-compose -f deployments/docker-compose.development.yml build camforchat-app
