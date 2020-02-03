d354313c0f3c3a4c7db6ffe7227c7ba7907f4a8d
Andrew N. Shalaev <isqad88@gmail.com>
2020-02-03 16:13:41 +0500

    avoid data race in test

--- broadcast_handler_test.go
+++ broadcast_handler_test.go
@@ -1,21 +1,31 @@
 package camforchat
 
 import (
+	"context"
 	"testing"
+	"time"
 )
 
 func TestStopView(t *testing.T) {
-	bh := NewBroadcastHandler()
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
-	var viewerID int64
+	bh := NewBroadcastHandler()
+	viewerIDs := make(chan int64)
 
 	go func() {
-		viewerID = <-bh.StopSubscribe
+		ID := <-bh.StopSubscribe
+		viewerIDs <- ID
 	}()
 
 	bh.StopView(1)
 
-	if viewerID != 1 {
-		t.Fatalf("Expected viewerID is eq 1, but not: %v", viewerID)
+	select {
+	case viewerID := <-viewerIDs:
+		if viewerID != 1 {
+			t.Fatalf("Expected viewerID is eq 1, but not: %v", viewerID)
+		}
+	case <-ctx.Done():
+		t.Fatalf("Timeout: %v", ctx.Err())
 	}
 }
