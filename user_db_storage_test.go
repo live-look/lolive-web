b44bdb918901f4c302f18f4a57da90e36f26925d
Andrew N. Shalaev <isqad88@gmail.com>
2020-02-05 07:43:39 +0500

    feature: add tests for user storage Load()

--- user_db_storage_test.go
+++ user_db_storage_test.go
@@ -95,3 +95,40 @@ func TestUserDbStorageSave(t *testing.T) {
 
 	assert.Equal(t, authboss.ErrUserNotFound, s.Save(context.Background(), user))
 }
+
+func TestUserDbStorageLoad(t *testing.T) {
+	spec := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_DB"))
+
+	db, err := NewDb(spec)
+	assert.Nil(t, err)
+
+	s := NewUserDbStorage(db)
+	_, err = s.Load(context.Background(), "thisuserdoesnotexist@example.com")
+
+	assert.Equal(t, authboss.ErrUserNotFound, err)
+
+	user := &User{
+		Name:            "foo",
+		Email:           "foo3@example.com",
+		Confirmed:       true,
+		Password:        "qwerty",
+		ConfirmSelector: "fizzbuzz",
+		ConfirmVerifier: "aaa",
+	}
+
+	err = s.Create(context.Background(), user)
+	assert.Nil(t, err)
+
+	usr, err := s.Load(context.Background(), "foo3@example.com")
+	assert.Nil(t, err)
+
+	user = usr.(*User)
+
+	assert.Equal(t, "foo", user.GetName())
+	assert.Equal(t, "foo3@example.com", user.GetEmail())
+	assert.True(t, user.GetConfirmed())
+}
